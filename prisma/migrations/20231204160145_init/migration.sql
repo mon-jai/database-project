0027e52dc7d386b6c56439334d3dc7ab3d8e7b1a
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-12-02 04:32:30 +0800

    Update schema.prisma

---
+++ prisma/migrations/20231204160145_init/migration.sql
@@ -0,0 +1,115 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('Customer', 'Admin');
+
+-- CreateEnum
+CREATE TYPE "Sex" AS ENUM ('Male', 'Female');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "user_state" TEXT NOT NULL,
+    "permission" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Customer" (
+    "userId" INTEGER NOT NULL,
+    "birthday" TIMESTAMP(3) NOT NULL,
+    "sex" "Sex" NOT NULL,
+    "address" TEXT NOT NULL,
+    "security_code" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "CreditCard" (
+    "id" SERIAL NOT NULL,
+    "customerUserId" INTEGER NOT NULL,
+    "card_no" TEXT NOT NULL,
+    "security_code" TEXT NOT NULL,
+    "expiration_year" TEXT NOT NULL,
+    "expiration_month" TEXT NOT NULL,
+
+    CONSTRAINT "CreditCard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "published_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modified_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "title" TEXT NOT NULL,
+    "cetegory" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "images" TEXT[],
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "customerUserId" INTEGER NOT NULL,
+    "ordered_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "shipped_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "payment_method" TEXT NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "orderId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "price_at_the_time" DOUBLE PRECISION NOT NULL,
+    "quantity" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "ShoppingCart" (
+    "customerUserId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_user_name_key" ON "User"("user_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_userId_key" ON "Customer"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "OrderItem_orderId_productId_key" ON "OrderItem"("orderId", "productId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ShoppingCart_customerUserId_productId_key" ON "ShoppingCart"("customerUserId", "productId");
+
+-- AddForeignKey
+ALTER TABLE "Customer" ADD CONSTRAINT "Customer_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CreditCard" ADD CONSTRAINT "CreditCard_customerUserId_fkey" FOREIGN KEY ("customerUserId") REFERENCES "Customer"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_customerUserId_fkey" FOREIGN KEY ("customerUserId") REFERENCES "Customer"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShoppingCart" ADD CONSTRAINT "ShoppingCart_customerUserId_fkey" FOREIGN KEY ("customerUserId") REFERENCES "Customer"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShoppingCart" ADD CONSTRAINT "ShoppingCart_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
