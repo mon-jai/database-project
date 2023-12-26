5a66fd1bcee5fa710216abc74e3592bdba84da15
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-12-27 04:56:21 +0800

    .

--- app/signin/api/route.ts
+++ app/signin/api/route.ts
@@ -1,23 +1,17 @@
-import { IRON_SESSION_PASSWORD } from "@/lib/constants"
-import { isPasswordCorrect } from "@/lib/utils"
+import { getSession, isPasswordCorrect } from "@/lib/utils"
 import { User } from "@prisma/client"
-import { getIronSession } from "iron-session"
-import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
   const body: User = await request.json()
 
   if (await isPasswordCorrect(body.username, body.password)) {
-    const response = new Response("Success", { status: 200 })
-    const session = await getIronSession(request, response, {
-      cookieName: "user-session",
-      password: IRON_SESSION_PASSWORD,
-      cookieOptions: { secure: !(process.env.NODE_ENV === "development") }
-    })
+    const session = await getSession()
+
+    session.username = body.username
     await session.save()
 
-    return response
+    return new Response("Success", { status: 200 })
   } else {
-    return NextResponse.json("", { status: 400 })
+    return new Response("", { status: 401 })
   }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,8 @@
+import { IRON_SESSION_PASSWORD } from "./constants"
 import prisma from "@/lib/prisma"
 import { compare, hash } from "bcrypt"
+import { getIronSession } from "iron-session"
+import { cookies } from "next/headers"
 
 export async function hashPassword(password: string) {
   return hash(password, 2)
@@ -10,4 +13,12 @@ export async function isPasswordCorrect(username: string, password: string) {
   if (passwordInDatabase === undefined) return false
 
   return compare(password, passwordInDatabase)
+}
+
+export async function getSession() {
+  return getIronSession<{ username: string }>(cookies(), {
+    cookieName: "user-session",
+    password: IRON_SESSION_PASSWORD,
+    cookieOptions: { secure: !(process.env.NODE_ENV === "development") }
+  })
 }
