24f68099bf628c8d9230fa606c1683ebb5347601
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-12-30 17:28:51 +0800

    Update pchome-scaper.js

--- prisma/pchome-scaper.js
+++ prisma/pchome-scaper.js
@@ -1,15 +1,18 @@
-import { writeFile } from "fs/promises"
-import { dirname, resolve } from "path"
-import { fileURLToPath } from "url"
+import { writeFile } from "fs/promises";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
 
-import { queue } from "async"
-import puppeteer from "puppeteer"
+
+
+import { queue } from "async";
+import puppeteer from "puppeteer";
+
 
 const sleep = ms => new Promise(r => setTimeout(r, ms))
 
 const browser = await puppeteer.launch({ headless: "new" })
 const outputPath = resolve(dirname(fileURLToPath(import.meta.url)), "products.json")
-const products = []
+const productPromises = []
 let fetchedCount = 0
 
 const page = await browser.newPage()
@@ -18,32 +21,36 @@ const productUrls = await page.$$eval("a[href^='https://24h.pchome.com.tw/prod']
 
 const fetchQueue = queue(async productUrl => {
   const page = await browser.newPage()
+  const localFetchCount = fetchedCount++
+
   page.goto(productUrl)
   await sleep(5000)
 
   try {
     const imageUrls = await page.$$eval(".c-radiusPhotoImage:first-child img", imgs => imgs.map(img => img.src))
-
-    products.push({
+    const product = {
+      id: localFetchCount,
       name: await page.$eval(".o-prodMainName", el => el.innerText),
       category: await page.$eval(".c-tagLink", el => el.innerText.substring(1)),
       price: parseFloat(await page.$eval(".o-prodPrice__price", el => el.innerText.replaceAll(/[\$,]/g, ""))),
       stock: 10,
-      description: await page.$eval(".c-prodBriefing__slogan", el => el.innerText),
-      images: await Promise.all(
+      description: await page.$eval(".c-prodBriefing__slogan", el => el.innerText)
+    }
+
+    productPromises.push(
+      Promise.all(
         imageUrls.map(async imageUrl => {
           const response = await fetch(imageUrl)
           const arrayBuffer = await response.arrayBuffer()
           return `data:${response.headers.get("Content-Type")};base64,${Buffer.from(arrayBuffer).toString("base64")}`
         })
-      )
-    })
-  } catch (e) {
-    console.log(productUrl)
-  }
+      ).then(images => ({ ...product, images }))
+    )
 
-  fetchedCount++
-  console.log(`Fetching products: ${fetchedCount} / ${productUrls.length}`)
+    console.log(`Fetched products: ${localFetchCount} / ${productUrls.length}`)
+  } catch (e) {
+    console.log(`Error: ${localFetchCount} / ${productUrls.length}, ${productUrl}`)
+  }
 
   // Avoid hitting rate limit
   await sleep(3000)
@@ -51,5 +58,6 @@ const fetchQueue = queue(async productUrl => {
 await fetchQueue.push(productUrls)
 await fetchQueue.drain()
 
+const products = await Promise.all(productPromises)
 await writeFile(outputPath, JSON.stringify(products))
 await browser.close()
