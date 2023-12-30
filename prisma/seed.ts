02d1acf4d1ad53532c98df1cb379714900db4059
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-12-31 01:56:35 +0800

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,9 @@ import _products from "./products.json" with { "type": "json" }
 const products = _products as any as Parameters<typeof prisma.product.create>[0]["data"][]
 
 try {
-  const response = await Promise.all(products.map(product => prisma.product.create({ data: product })))
+  const response = await Promise.all(
+    products.map(product => prisma.product.upsert({ where: { id: product.id }, update: {}, create: product }))
+  )
   console.log(response)
 } catch (e) {
   console.error(e)
